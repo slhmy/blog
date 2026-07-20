dc3632e9cfd0900a4ee0d579e929a488a7b7a58b
slhmy <slhmy.zzy@gmail.com>
2026-07-20 15:45:28 +0800

    Add stock entry for July 20, 2026

--- src/data/stock.ts
+++ src/data/stock.ts
@@ -8,4 +8,5 @@ export const stockEntries: StockEntry[] = [
 	{ date: '2026-07-15', value: -2311.45 },
 	{ date: '2026-07-16', value: -1658.8 },
 	{ date: '2026-07-17', value: -277.26 },
+	{ date: '2026-07-20', value: 567.41 },
 ];
